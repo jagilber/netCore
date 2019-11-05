eb8f97c8ffcbd96dd56ad360e33ac0bd608e2390
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-11-04 19:22:42 -0500

    cleanup

--- netCoreMsalCliTokenCache/Program.cs
+++ netCoreMsalCliTokenCache/Program.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Linq;
 using System.Security.Cryptography;
 using Microsoft.Identity.Client;
@@ -11,14 +13,16 @@ namespace netCoreMsalTokenCacheCli
 {
     internal class Program
     {
-        public static string ClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
-        public static bool Force = false;
-        public static IPublicClientApplication PublicClientApp;
-        public static string RedirectUri = "http://localhost";
-        public static string Resource = null;
-        public static string TenantId = "common";
+        private string ClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
+        private bool Detail = false;
+        private bool Force = false;
+        private bool Help = false;
+        private IPublicClientApplication PublicClientApp;
+        private string RedirectUri = "http://localhost";
+        private string Resource = null;
+        private string TenantId = "common";
 
-        public static List<string> Scope { get; set; } = new List<string>();
+        private List<string> Scope { get; set; } = new List<string>() { ".default" };
 
         private static void Main(string[] args)
         {
@@ -36,25 +40,32 @@ namespace netCoreMsalTokenCacheCli
         {
             for (int i = 0; i != args.Length; ++i)
             {
-                string arg = args[i].ToLower();
-                if (arg.StartsWith('/')) { arg = '-' + arg.TrimStart('/'); }
-                if (arg == "-?") { ShowHelp(); }
-                if (arg == "-resource") { Resource = args[i + 1]; }
-                if (arg == "-redirecturi") { RedirectUri = args[i + 1]; }
-                if (arg == "-clientid") { ClientId = args[i + 1]; }
-                if (arg == "-tenantid") { TenantId = args[i + 1]; }
-                if (arg == "-scope") { Scope.Add(args[i + 1]); }
+                string arg = '-' + args[i].ToLower().TrimStart(new char[] { '-', '/' });
+                if (arg == "-resource") { Resource = args[++i]; }
+                if (arg == "-redirecturi") { RedirectUri = args[++i]; }
+                if (arg == "-clientid") { ClientId = args[++i]; }
+                if (arg == "-tenantid") { TenantId = args[++i]; }
                 if (arg == "-force") { Force = true; }
+                if (arg == "-detail") { Detail = true; }
+                if (arg == "-?") { Help = true; }
+                if (arg == "-scope")
+                {
+                    Scope.Clear();
+                    Scope.AddRange(args[++i].Split(','));
+                }
             }
 
-            if (Scope.Count < 1)
+            if (Help)
             {
-                Scope.Add(".default");
+                Console.WriteLine("// requires -resource argument. optional -redirectUri -clientId -tenantId -scope -detail");
+                Console.WriteLine("// run from non administrator prompt!");
+                ShowDetail();
+                return;
             }
 
-            if (args.Length < 2)
+            if (Detail)
             {
-                ShowHelp();
+                ShowDetail();
             }
 
             Authorize();
@@ -83,65 +94,112 @@ namespace netCoreMsalTokenCacheCli
                     .ExecuteAsync().Result;
             }
 
-            Console.WriteLine(authenticationResult.AccessToken);
+            FormatJsonOutput(authenticationResult);
         }
 
-        private void ShowHelp()
+        private static void FormatJsonOutput(AuthenticationResult authenticationResult)
         {
-            Console.WriteLine($"scope: {Scope[0].ToString()}");
-            Console.WriteLine($"resource: {Resource}");
-            Console.WriteLine($"clientID: {ClientId}");
-            Console.WriteLine($"RedirectUri: {RedirectUri}");
-            Console.WriteLine($"tenantID: {TenantId}");
-            Console.WriteLine("");
-            Console.WriteLine("requires -resource argument. optional -redirectUri -clientId -tenantId -scope");
-            Console.WriteLine("run from non administrator prompt!");
+            JsonWriterOptions options = new JsonWriterOptions{ Indented = true };
 
-        }
-        static class TokenCacheHelper
-        {
-            public static void EnableSerialization(ITokenCache tokenCache)
+            using (MemoryStream stream = new MemoryStream())
             {
-                tokenCache.SetBeforeAccess(BeforeAccessNotification);
-                tokenCache.SetAfterAccess(AfterAccessNotification);
+                using (Utf8JsonWriter writer = new Utf8JsonWriter(stream, options))
+                {
+                    writer.WriteStartObject();
+                    writer.WriteString("AccessToken", authenticationResult.AccessToken);
+
+                    writer.WriteStartObject("Account");
+                    writer.WriteString("Environment", authenticationResult.Account.Environment);
+
+                    writer.WriteStartObject("HomeAccountId");
+                    writer.WriteString("Identifier", authenticationResult.Account.HomeAccountId.Identifier);
+                    writer.WriteString("ObjectId", authenticationResult.Account.HomeAccountId.ObjectId);
+                    writer.WriteString("TenantId", authenticationResult.Account.HomeAccountId.TenantId);
+                    writer.WriteEndObject();
+
+                    writer.WriteString("Username", authenticationResult.Account.Username);
+                    writer.WriteEndObject();
+
+                    writer.WriteString("CorrelationId", authenticationResult.CorrelationId);
+                    writer.WriteString("ExpiresOn", authenticationResult.ExpiresOn);
+                    writer.WriteString("ExtendedExpiresOn", authenticationResult.ExtendedExpiresOn);
+                    writer.WriteString("IdToken", authenticationResult.IdToken);
+                    writer.WriteBoolean("IsExtendedLifeTimeToken", authenticationResult.IsExtendedLifeTimeToken);
+
+                    writer.WriteStartArray("Scopes");
+                    foreach (string scope in authenticationResult.Scopes)
+                    {
+                        writer.WriteStringValue(scope);
+                    }
+                    writer.WriteEndArray();
+
+                    writer.WriteString("TenantId", authenticationResult.TenantId);
+                    writer.WriteString("UniqueId", authenticationResult.UniqueId);
+                    writer.WriteEndObject();
+                }
+
+                string json = Encoding.UTF8.GetString(stream.ToArray());
+                Console.WriteLine(json);
             }
+        }
 
-            /// <summary>
-            /// Path to the token cache
-            /// </summary>
-            public static readonly string CacheFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalcache.bin3";
+        private void ShowDetail()
+        {
+            Console.WriteLine("//");
+            Console.WriteLine($"// scope: {string.Join(" ", Scope)}");
+            Console.WriteLine($"// resource: {Resource}");
+            Console.WriteLine($"// clientID: {ClientId}");
+            Console.WriteLine($"// RedirectUri: {RedirectUri}");
+            Console.WriteLine($"// tenantID: {TenantId}");
+            Console.WriteLine("//");
+        }
 
-            private static readonly object FileLock = new object();
+    }
+
+    public static class TokenCacheHelper
+    {
+        public static void EnableSerialization(ITokenCache tokenCache)
+        {
+            tokenCache.SetBeforeAccess(BeforeAccessNotification);
+            tokenCache.SetAfterAccess(AfterAccessNotification);
+        }
+
+        /// <summary>
+        /// Path to the token cache
+        /// </summary>
+        public static readonly string CacheFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalcache.bin3";
+
+        public static readonly object FileLock = new object();
 
 
-            private static void BeforeAccessNotification(TokenCacheNotificationArgs args)
+        public static void BeforeAccessNotification(TokenCacheNotificationArgs args)
+        {
+            lock (FileLock)
+            {
+                args.TokenCache.DeserializeMsalV3(File.Exists(CacheFilePath)
+                        ? ProtectedData.Unprotect(File.ReadAllBytes(CacheFilePath),
+                                                  null,
+                                                  DataProtectionScope.CurrentUser)
+                        : null);
+            }
+        }
+
+        public static void AfterAccessNotification(TokenCacheNotificationArgs args)
+        {
+            // if the access operation resulted in a cache update
+            if (args.HasStateChanged)
             {
                 lock (FileLock)
                 {
-                    args.TokenCache.DeserializeMsalV3(File.Exists(CacheFilePath)
-                            ? ProtectedData.Unprotect(File.ReadAllBytes(CacheFilePath),
-                                                      null,
-                                                      DataProtectionScope.CurrentUser)
-                            : null);
-                }
-            }
-
-            private static void AfterAccessNotification(TokenCacheNotificationArgs args)
-            {
-                // if the access operation resulted in a cache update
-                if (args.HasStateChanged)
-                {
-                    lock (FileLock)
-                    {
-                        // reflect changesgs in the persistent store
-                        File.WriteAllBytes(CacheFilePath,
-                                            ProtectedData.Protect(args.TokenCache.SerializeMsalV3(),
-                                                                    null,
-                                                                    DataProtectionScope.CurrentUser)
-                                            );
-                    }
+                    // reflect changesgs in the persistent store
+                    File.WriteAllBytes(CacheFilePath,
+                                        ProtectedData.Protect(args.TokenCache.SerializeMsalV3(),
+                                                                null,
+                                                                DataProtectionScope.CurrentUser)
+                                        );
                 }
             }
         }
     }
+
 }
